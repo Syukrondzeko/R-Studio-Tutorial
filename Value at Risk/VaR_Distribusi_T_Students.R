c0800b44fb1ea8eca55da33124694ccebcdef5c7
Syukrondzeko <67746408+Syukrondzeko@users.noreply.github.com>
2020-09-13 07:44:49 +0700

    Create VaR_Distribusi_T_Students.R

---
+++ Value at Risk/VaR_Distribusi_T_Students.R
@@ -0,0 +1,21 @@
+library(readxl)
+Data_Mentah <- read_excel("C:/Users/HP PC/Downloads/5. Var Normal, T, dan Hist Simulation/Data_VaR.xlsx")
+View(Data_Mentah)
+Data_VaR=Data_Mentah[2:40,c(3,5,7)]
+View(Data_VaR)
+
+RTLKM=Data_VaR[,1]
+View(RTLKM)
+data=RTLKM$`r(TLKM)`
+n=length(data)
+
+#VaR T
+m=mean(data)
+s=sd(data)
+z=qnorm(0.05)
+t=qt(0.05,n)
+hp=1
+modal=1000000
+
+VaR_T = -(modal*sqrt(hp)*t*s*sqrt((n-2)/n))
+
